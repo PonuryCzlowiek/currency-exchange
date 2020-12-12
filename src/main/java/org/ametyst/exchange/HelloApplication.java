3e7c695510d39655f95a61a3b52fb1456620c8f5
przemyslaw.pilarski <przemyslaw.pilarski@jeppesen.com>
2020-12-12 18:23:31 +0100

    Initial commit

---
+++ src/main/java/org/ametyst/exchange/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.ametyst.exchange;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
