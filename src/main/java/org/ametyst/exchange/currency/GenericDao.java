ce64df0ccb9beff4a6c7c4524636771b5967ea67
przemyslaw.pilarski <przemyslaw.pilarski@jeppesen.com>
2020-12-14 22:24:01 +0100

    Working with real DB. Not clean yet

--- src/main/java/org/ametyst/exchange/currency/GenericDao.java
+++ src/main/java/org/ametyst/exchange/currency/GenericDao.java
@@ -1,29 +1,28 @@
 package org.ametyst.exchange.currency;
 
-import java.util.LinkedList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class GenericDao<T> {
-
-    List<T> list = new LinkedList<>();
     @PersistenceContext(unitName = "pu")
     EntityManager entityManager;
 
     T save(T object) {
-//        entityManager.persist(object);
-        list.add(object);
+        entityManager.persist(object);
         return object;
     }
 
     protected List<T> getAll(Class<T> entityClass) {
-//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-//        CriteriaQuery<T> cq = cb.createQuery(entityClass);
-//        Root<T> rootEntry = cq.from(entityClass);
-//        CriteriaQuery<T> all = cq.select(rootEntry);
-//        TypedQuery<T> allQuery = entityManager.createQuery(all);
-//        return allQuery.getResultList();
-        return list;
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> cq = cb.createQuery(entityClass);
+        Root<T> rootEntry = cq.from(entityClass);
+        CriteriaQuery<T> all = cq.select(rootEntry);
+        TypedQuery<T> allQuery = entityManager.createQuery(all);
+        return allQuery.getResultList();
     }
 }
--- src/main/java/org/ametyst/exchange/currency/Rate.java
+++ src/main/java/org/ametyst/exchange/currency/Rate.java
@@ -2,6 +2,7 @@ package org.ametyst.exchange.currency;
 
 import java.util.Date;
 import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -11,58 +12,63 @@ import javax.persistence.Table;
 public class Rate {
 
     @Id
-    private UUID id;
-    private String from;
-    private String to;
-    private Double rate;
-    private Date date;
-    private Date searchTimestamp;
+    @Column(length = 255)
+    private String id;
+    private String from_currency;
+    private String to_currency;
+    private Double currency_rate;
+    private Date search_date;
+    private Date search_timestamp;
 
-    public void setId(UUID id) {
+    public Rate() {
+        id = UUID.randomUUID().toString();
+    }
+
+    public void setId(String id) {
         this.id = id;
     }
 
-    public UUID getId() {
+    public String getId() {
         return id;
     }
 
-    public String getFrom() {
-        return from;
+    public String getFrom_currency() {
+        return from_currency;
     }
 
-    public void setFrom(String from) {
-        this.from = from;
+    public void setFrom_currency(String from) {
+        this.from_currency = from;
     }
 
-    public String getTo() {
-        return to;
+    public String getTo_currency() {
+        return to_currency;
     }
 
-    public void setTo(String to) {
-        this.to = to;
+    public void setTo_currency(String to) {
+        this.to_currency = to;
     }
 
-    public Double getRate() {
-        return rate;
+    public Double getCurrency_rate() {
+        return currency_rate;
     }
 
-    public void setRate(Double rate) {
-        this.rate = rate;
+    public void setCurrency_rate(Double rate) {
+        this.currency_rate = rate;
     }
 
-    public Date getDate() {
-        return date;
+    public Date getSearch_date() {
+        return search_date;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setSearch_date(Date date) {
+        this.search_date = date;
     }
 
-    public Date getSearchTimestamp() {
-        return searchTimestamp;
+    public Date getSearch_timestamp() {
+        return search_timestamp;
     }
 
-    public void setSearchTimestamp(Date searchTimestamp) {
-        this.searchTimestamp = searchTimestamp;
+    public void setSearch_timestamp(Date searchTimestamp) {
+        this.search_timestamp = searchTimestamp;
     }
 }
