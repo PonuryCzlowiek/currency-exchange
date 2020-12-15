3775180511b7269f25413cdff8bc95fe69416504
przemyslaw.pilarski <przemyslaw.pilarski@jeppesen.com>
2020-12-15 07:09:02 +0100

    Add tests

---
+++ src/test/java/org/ametyst/exchange/coverters/date/DateParameterConverterTest.java
@@ -0,0 +1,35 @@
+package org.ametyst.exchange.coverters.date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+class DateParameterConverterTest {
+
+    DateParameterConverter dateParameterConverter = new DateParameterConverter();
+
+    @Test
+    void shouldParseStringToDate() {
+        // arrange
+        String dateAsString = "1999-11-23";
+
+        // act
+        Date date = dateParameterConverter.fromString(dateAsString);
+
+        // assert
+        assertEquals(new Date(943311600000L), date);
+    }
+
+    @Test
+    void shouldParseDateToString() {
+        // arrange
+        Date date = new Date(943311600000L);
+
+        // act
+        String dateAsString = dateParameterConverter.toString(date);
+
+        // assert
+        assertEquals("1999-11-23", dateAsString);
+    }
+}
